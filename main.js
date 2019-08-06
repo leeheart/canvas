fd1997329c50ffc3c75077ea5ff72c578b5f1211
leehe <lihechn@outlook.com>
2019-08-06 17:10:22 +0800

    添加下载功能

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var yyy = document.getElementById('xxx')
 var context = yyy.getContext('2d')
+var lineWidth = 5
 
 autoSetCanvas(yyy)
 
@@ -9,13 +10,58 @@ listenToUser(yyy)
 /**************************/
 
 var eraserEnabled = false
-eraser.onclick = function () {
-  eraserEnabled = true
-  actions.className = 'actions x'
-}
-brush.onclick = function () {
+pen.onclick = function(){
   eraserEnabled = false
-  actions.className = 'actions'
+  pen.classList.add('active')
+  eraser.classList.remove('active')
+}
+eraser.onclick = function(){
+  eraserEnabled = true
+  eraser.classList.add('active')
+  pen.classList.remove('active')
+}
+
+red.onclick = function(){
+  context.fillStyle = 'red'
+  context.strokeStyle = 'red'
+  red.classList.add('active')
+  green.classList.remove('active')
+  blue.classList.remove('active')
+}
+green.onclick = function(){
+  context.fillStyle = 'green'
+  context.strokeStyle = 'green'
+  green.classList.add('active')
+  red.classList.remove('active')
+  blue.classList.remove('active')
+}
+blue.onclick = function(){
+  context.fillStyle = 'blue'
+  context.strokeStyle = 'blue'
+  blue.classList.add('active')
+  red.classList.remove('active')
+  green.classList.remove('active')
+}
+
+thin.onclick = function(){
+  lineWidth = 5
+}
+thick.onclick = function(){
+  lineWidth = 10
+}
+
+clear.onclick = function(){
+  context.clearRect(0,0,yyy.width,yyy.height)
+}
+
+save.onclick = function(){
+  var url = yyy.toDataURL("image/png")
+  var a = document.createElement('a')
+  a.href = url
+  a.target = '_blank'
+  a.download = '我的画'
+  document.body.appendChild(a)
+  a.click()
 }
 
 
@@ -39,17 +85,15 @@ function autoSetCanvas(canvas) {
 
 function drawCircle(x, y, radius) {
   context.beginPath();
-  context.fillStyle = 'black'
   context.arc(x, y, radius, 0, 2 * Math.PI);
   context.fill();
 }
 
 function drawLine(x1, y1, x2, y2) {
   context.beginPath();
-  context.strokeStyle = 'black'
   context.moveTo(x1, y1); //起点
   context.lineTo(x2, y2); //终点
-  context.lineWidth = 5;
+  context.lineWidth = lineWidth;
   context.stroke();
   context.closePath();
 }
